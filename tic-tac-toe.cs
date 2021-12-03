722559e4f954970453ca18a1ca3f0c36a2306f6a
n-kev1n <kevin_@tuta.io>
2021-12-03 21:37:03 +0100

    Added Board, Added Checking

--- tic-tac-toe.cs
+++ tic-tac-toe.cs
@@ -5,8 +5,93 @@ namespace TicTacToe
 	class Game
 	{
 		static void Main(string[] args){
-			// Code goes here
-			Console.WriteLine("Hello TicTacToe!");
+
+			// Board Array...
+			string[] board = {"-", "-", "-",
+							  "-", "-", "-",
+							  "-", "-", "-"};
+
+			// Game Characters...
+			string playerOne = "X";
+			string playerTwo = "O";
+			string winner;
+
+			// User Inputs...
+			int userInput1;
+			int userInput2;
+
+			// Game Flag (The Controller of the Game)...
+			bool gameFlag = true;
+
+
+			while (gameFlag)
+			{
+				Console.Write("X\'s Move > ");
+				userInput1 = Convert.ToInt32(Console.ReadLine());
+
+				if (userInput1 >= 1 && userInput1 <= 9 && board[userInput1-1] == "-" )
+				{
+					board[userInput1-1] = playerOne;
+				}
+				else
+				{
+					Console.WriteLine("Invalid Move");
+				}
+				// Printing the Game Board...
+				Console.WriteLine( "\n  " + board[0] + " | " + board[1] + " | " + board[2]);
+				Console.WriteLine(" ---+---+---");
+				Console.WriteLine( "  " + board[3] + " | " + board[4] + " | " + board[5]);
+				Console.WriteLine(" ---+---+---");
+				Console.WriteLine( "  " + board[6] + " | " + board[7] + " | " + board[8]);
+
+				// Checking For Column...
+				if (board[0] == board[1] == board[2] && board[1] != "-")
+				{
+					winner = board[0];
+					gameFlag = false;
+				}
+				else if (board[3] == board[4] == board[5] && board[3] != "-")
+				{
+					winner = board[3];
+					gameFlag = false;
+				}
+				else if (board[6] == board[7] == board[8] && board[6] != "-")
+				{
+					winner = board[6];
+					gameFlag = false;
+				}
+
+				// Checking For Row...
+				if (board[0] == board[3] == board[6] && board[0] != "-")
+				{
+					winner = board[0];
+					gameFlag = false;
+				}
+				else if (board[1] == board[4] == board[7] && board[1] != "-")
+				{
+					winner = board[3];
+					gameFlag = false;
+				}
+				else if (board[2] == board[5] == board[8] && board[2] != "-")
+				{
+					winner = board[6];
+					gameFlag = false;
+				}
+
+				// Checking For Diagonal...
+				if (board[0] == board[4] == board[8] && board[0] != "-")
+				{
+					winner = board[0];
+					gameFlag = false;
+				}
+				else if (board[2] == board[4] == board[6] && board[2] != "-")
+				{
+					winner = board[3];
+					gameFlag = false;
+				}
+
+			}
+
 		}
 	}
 }
